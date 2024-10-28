9bbca0372e3b7963b64e174d3c3b52013f385680
as <as@asdeiMac.local>
2024-10-28 15:47:10 +0800

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using System.Management;
 
 namespace SystemInfo
@@ -13,37 +11,39 @@ namespace SystemInfo
             string cpuInfo = GetCpuInfo();
             Console.WriteLine($"CPU 型号: {cpuInfo}");
 
-            // 获取内存大小
-            long memorySize = GetMemorySize();
-            Console.WriteLine($"内存大小: {memorySize / (1024 * 1024)} MB");
+            // 获取总内存大小
+            long memorySize = GetTotalPhysicalMemory();
+            Console.WriteLine($"内存大小: {memorySize} MB");
 
             // 获取磁盘容量
             GetDiskInfo();
 
-            Console.ReadLine();
+            // 等待用户输入，防止窗口关闭
+            Console.WriteLine("按任意键退出...");
             Console.ReadKey();
         }
 
         static string GetCpuInfo()
         {
+            string cpuInfo = string.Empty;
             using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select Name from Win32_Processor"))
             {
                 foreach (ManagementObject obj in searcher.Get())
                 {
-                    return obj["Name"].ToString();
+                    cpuInfo = obj["Name"].ToString();
                 }
             }
-            return "无法获取 CPU 信息";
+            return cpuInfo;
         }
 
-        static long GetMemorySize()
+        static long GetTotalPhysicalMemory()
         {
             long totalMemory = 0;
             using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select Capacity from Win32_PhysicalMemory"))
             {
                 foreach (ManagementObject obj in searcher.Get())
                 {
-                    totalMemory += Convert.ToInt64(obj["Capacity"]); // 累加每个内存条的容量
+                    totalMemory += Convert.ToInt64(obj["Capacity"]);
                 }
             }
             return totalMemory / (1024 * 1024); // 转换为 MB
@@ -51,12 +51,16 @@ namespace SystemInfo
 
         static void GetDiskInfo()
         {
-            DriveInfo[] drives = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in drives)
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select DeviceID, Size, FreeSpace from Win32_LogicalDisk"))
             {
-                if (drive.IsReady)
+                foreach (ManagementObject obj in searcher.Get())
                 {
-                    Console.WriteLine($"磁盘 {drive.Name}: {drive.TotalSize / (1024 * 1024)} MB 可用: {drive.AvailableFreeSpace / (1024 * 1024)} MB");
+                    string deviceId = obj["DeviceID"].ToString();
+                    long size = Convert.ToInt64(obj["Size"]);
+                    long freeSpace = Convert.ToInt64(obj["FreeSpace"]);
+                    long usedSpace = size - freeSpace;
+
+                    Console.WriteLine($"磁盘 {deviceId} - 总容量: {size / (1024 * 1024)} MB, 可用空间: {freeSpace / (1024 * 1024)} MB, 已用空间: {usedSpace / (1024 * 1024)} MB");
                 }
             }
         }
