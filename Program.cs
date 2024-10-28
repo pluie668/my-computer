830245e36af5a798152a753d7d2c03c54fa42668
as <as@asdeiMac.local>
2024-10-28 16:10:08 +0800

    test2

--- Program.cs
+++ Program.cs
@@ -1,71 +1,118 @@
 ﻿using System;
-using Microsoft.Management.Infrastructure;
+using Hardware.Info;
+class Program
+ {
+     static IHardwareInfo hardwareInfo;
 
-namespace SystemInfo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // 获取 CPU 型号
-            string cpuInfo = GetCpuInfo();
-            Console.WriteLine($"CPU 型号: {cpuInfo}");
+     static void Main(string[] _)
+     {
+         try
+         {
+             hardwareInfo = new HardwareInfo();
 
-            // 获取总内存大小
-            long memorySize = GetTotalPhysicalMemory();
-            Console.WriteLine($"内存大小: {memorySize} MB");
+             //hardwareInfo.RefreshOperatingSystem();
+             //hardwareInfo.RefreshMemoryStatus();
+             //hardwareInfo.RefreshBatteryList();
+             //hardwareInfo.RefreshBIOSList();
+             //hardwareInfo.RefreshComputerSystemList();
+             //hardwareInfo.RefreshCPUList();
+             //hardwareInfo.RefreshDriveList();
+             //hardwareInfo.RefreshKeyboardList();
+             //hardwareInfo.RefreshMemoryList();
+             //hardwareInfo.RefreshMonitorList();
+             //hardwareInfo.RefreshMotherboardList();
+             //hardwareInfo.RefreshMouseList();
+             //hardwareInfo.RefreshNetworkAdapterList();
+             //hardwareInfo.RefreshPrinterList();
+             //hardwareInfo.RefreshSoundDeviceList();
+             //hardwareInfo.RefreshVideoControllerList();
 
-            // 获取磁盘容量
-            GetDiskInfo();
+             hardwareInfo.RefreshAll();
+         }
+         catch (Exception ex)
+         {
+             Console.WriteLine(ex);
+         }
 
-            // 等待用户输入，防止窗口关闭
-            Console.WriteLine("按任意键退出...");
-            Console.ReadKey();
-        }
+         Console.WriteLine(hardwareInfo.OperatingSystem);
 
-        static string GetCpuInfo()
-        {
-            string cpuInfo = string.Empty;
-            using (var cimSession = CimSession.Create("localhost"))
-            {
-                var instances = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT Name FROM Win32_Processor");
-                foreach (var instance in instances)
-                {
-                    cpuInfo = instance.CimInstanceProperties["Name"].Value.ToString();
-                }
-            }
-            return cpuInfo;
-        }
+         Console.WriteLine(hardwareInfo.MemoryStatus);
 
-        static long GetTotalPhysicalMemory()
-        {
-            long totalMemory = 0;
-            using (var cimSession = CimSession.Create("localhost"))
-            {
-                var instances = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT Capacity FROM Win32_PhysicalMemory");
-                foreach (var instance in instances)
-                {
-                    totalMemory += Convert.ToInt64(instance.CimInstanceProperties["Capacity"].Value);
-                }
-            }
-            return totalMemory / (1024 * 1024); // 转换为 MB
-        }
+         foreach (var hardware in hardwareInfo.BatteryList)
+             Console.WriteLine(hardware);
 
-        static void GetDiskInfo()
-        {
-            using (var cimSession = CimSession.Create("localhost"))
-            {
-                var instances = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT DeviceID, Size, FreeSpace FROM Win32_LogicalDisk");
-                foreach (var instance in instances)
-                {
-                    string deviceId = instance.CimInstanceProperties["DeviceID"].Value.ToString();
-                    long size = Convert.ToInt64(instance.CimInstanceProperties["Size"].Value);
-                    long freeSpace = Convert.ToInt64(instance.CimInstanceProperties["FreeSpace"].Value);
-                    long usedSpace = size - freeSpace;
+         foreach (var hardware in hardwareInfo.BiosList)
+             Console.WriteLine(hardware);
 
-                    Console.WriteLine($"磁盘 {deviceId} - 总容量: {size / (1024 * 1024)} MB, 可用空间: {freeSpace / (1024 * 1024)} MB, 已用空间: {usedSpace / (1024 * 1024)} MB");
-                }
-            }
-        }
-    }
-}
+         foreach (var hardware in hardwareInfo.ComputerSystemList)
+             Console.WriteLine(hardware);
+
+         foreach (var cpu in hardwareInfo.CpuList)
+         {
+             Console.WriteLine(cpu);
+
+             foreach (var cpuCore in cpu.CpuCoreList)
+                 Console.WriteLine(cpuCore);
+         }
+
+         foreach (var drive in hardwareInfo.DriveList)
+         {
+             Console.WriteLine(drive);
+
+             foreach (var partition in drive.PartitionList)
+             {
+                 Console.WriteLine(partition);
+
+                 foreach (var volume in partition.VolumeList)
+                     Console.WriteLine(volume);
+             }
+         }
+
+         foreach (var hardware in hardwareInfo.KeyboardList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.MemoryList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.MonitorList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.MotherboardList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.MouseList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.NetworkAdapterList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.PrinterList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.SoundDeviceList)
+             Console.WriteLine(hardware);
+
+         foreach (var hardware in hardwareInfo.VideoControllerList)
+             Console.WriteLine(hardware);
+
+         foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Ethernet, OperationalStatus.Up))
+             Console.WriteLine(address);
+
+         Console.WriteLine();
+
+         foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Wireless80211))
+             Console.WriteLine(address);
+
+         Console.WriteLine();
+
+         foreach (var address in HardwareInfo.GetLocalIPv4Addresses(OperationalStatus.Up))
+             Console.WriteLine(address);
+
+         Console.WriteLine();
+
+         foreach (var address in HardwareInfo.GetLocalIPv4Addresses())
+             Console.WriteLine(address);
+
+         Console.ReadLine();
+     }
+ }
